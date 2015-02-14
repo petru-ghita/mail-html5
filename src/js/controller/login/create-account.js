8bea1ea717f17a9dde21fc1ddf527bc20a98624d
Tankred Hase <tankred@whiteout.io>
2015-02-14 16:07:04 +0100

    Add login to wmail on singup screen

--- src/js/controller/login/create-account.js
+++ src/js/controller/login/create-account.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var CreateAccountCtrl = function($scope, $location, $routeParams, $q, auth, admin, appConfig) {
+var CreateAccountCtrl = function($scope, $location, $routeParams, $q, auth, admin, appConfig, mailConfig) {
     !$routeParams.dev && !auth.isInitialized() && $location.path('/'); // init app
 
     // init phone region
     $scope.region = 'DE';
+    $scope.domain = '@' + appConfig.config.wmailDomain;
 
     $scope.createWhiteoutAccount = function() {
         if ($scope.form.$invalid) {
@@ -19,7 +20,7 @@ var CreateAccountCtrl = function($scope, $location, $routeParams, $q, auth, admi
 
         }).then(function() {
             // read form values
-            var emailAddress = $scope.user + '@' + appConfig.config.wmailDomain;
+            var emailAddress = $scope.user + $scope.domain;
             var phone = PhoneNumber.Parse($scope.dial, $scope.region);
             if (!phone || !phone.internationalNumber) {
                 throw new Error('Invalid phone number!');
@@ -48,6 +49,29 @@ var CreateAccountCtrl = function($scope, $location, $routeParams, $q, auth, admi
         }).catch(function(err) {
             $scope.busy = false;
             $scope.errMsg = err.errMsg || err.message;
+        });
+    };
+
+    $scope.loginToExisting = function() {
+        return $q(function(resolve) {
+            $scope.busy = true;
+            $scope.errMsg = undefined; // reset error msg
+            resolve();
+
+        }).then(function() {
+            return mailConfig.get('user' + $scope.domain);
+
+        }).then(function(config) {
+            $scope.busy = false;
+            $scope.state.login = {
+                mailConfig: config
+            };
+
+            $location.path('/login-set-credentials');
+
+        }).catch(function() {
+            $scope.busy = false;
+            $scope.errMsg = 'Error fetching IMAP settings for wmail.io!';
         });
     };
 };
